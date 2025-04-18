02bff9f1c3b27b6cc1e449c9be711fcacfcaaec9
kudachi <mekimeki2.net@gmail.com>
2025-04-18 18:03:04 +0900

    feat: add theme toggle and greeting component; update dependencies
    - Added ThemeIcon component for dark/light theme toggle functionality.
    - Introduced Greeting component to display random greetings.
    - Updated package.json to include @astrojs/preact and preact dependencies.
    - Modified Header component to include ThemeIcon.
    - Changed about page to set finished state to true.
    - Updated index page to include Greeting component with messages.
    - Enhanced global styles for dark theme support.
    - Updated tsconfig.json to support Preact with JSX.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://reliable-halva-ff78ed.netlify.app/"
+  site: "https://reliable-halva-ff78ed.netlify.app/",
+  integrations: [preact()]
 });
